d8995d2f61e33119d25dc2cb8f39c844bd52b508
sanmeranam@gmail.com <>
2017-07-24 22:33:43 +0530

    next chagnes

--- public_html/js/add.controller.js
+++ public_html/js/add.controller.js
@@ -1,29 +1,26 @@
 angular.module('appCherryPick')
-        .controller("addController", function ($scope, $http, $sce) {
-            $scope.dynamicPopover = {
-                content: 'Hello, World!',
-                templateUrl: 'myPopoverTemplate.html',
-                title: 'Title'
-            };
+        .controller("addController", function ($scope, BBService, DBService) {
+            $scope.project_name = "RDM-FOR-JCP";
+            $scope.project_internal = "RDMFORJCP";
 
             $scope.source_repo = "";
             $scope.source_commit = "";
             $scope.source_branch = "";
-            $scope.source_repos = ["A", "B", "C"];
-            
+            $scope.source_repos = [];
+
             $scope.target_repo = "";
             $scope.target_tag = "";
-            $scope.target_repos = ["A", "B", "C"];
+            $scope.target_repos = [];
 
             $scope.selectSourceRepo = function (repo) {
                 $scope.source_repo = repo;
                 $scope.isSourcePopupOpen = false;
             };
-            
+
             $scope.selectSourceCommit = function (repo) {
                 $scope.source_commit = repo;
             };
-             $scope.deselectSourceCommit = function () {
+            $scope.deselectSourceCommit = function () {
                 $scope.source_commit = null;
             };
 
@@ -32,12 +29,36 @@ angular.module('appCherryPick')
                 $scope.isTargetPopupOpen = false;
             };
 
-            $scope.source_load_repos = function () {
-
+            $scope.selectTargetTag = function (tag) {
+                $scope.target_tag = tag;
+            };
+            $scope.deselectTargetTag = function () {
+                $scope.target_tag = null;
             };
 
-            $scope.target_load_repos = function () {
 
+            $scope.load_repos = function (project) {
+                $scope.load_repos_status = true;
             };
+            
+
+            $scope.load_tags = function (repo) {
+                $scope.load_tags_status = false;
+            };
+
+            $scope.load_commits = function (repo) {
+                $scope.load_commits_status = false;
+            };
+
+            $scope.load_commit_info = function (commitId) {
+                $scope.load_commit_info_status = false;
+            };
+
+            $scope.load_commit_branch = function (commitId) {
+                $scope.load_commit_branch_status = false;
+            };
+            
+            
+            $scope.load_repos();
 
         });
--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,3 +1,28 @@
 (function () {
-    angular.module('appCherryPick', ['ui.bootstrap', 'ngSanitize']);
+    angular.module('appCherryPick', ['ui.bootstrap', 'ngSanitize'])
+            .service("BBService", function ($http) {
+                return {
+                    getRepos: function (ProjectName) {
+                        return $http.get("");
+                    },
+                    getTags: function (RepoName) {
+                        return $http.get("");
+                    },
+                    getCommits: function (RepoName) {
+                        return $http.get("");
+                    },
+                    getCommitStatus: function (commitId) {
+                        return $http.get("");
+                    },
+                    getCommitBranch: function (commitId) {
+                        return $http.get("");
+                    }
+                };
+            })
+            .service("DBService", function ($http) {
+                return {
+                    
+                };
+            });
+
 })();
